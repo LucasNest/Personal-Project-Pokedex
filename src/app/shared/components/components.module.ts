ea22a8c975fbbf85622a5d1a3c1d7268e40bbcee
LucasNest <lucasl0gc01@gmail.com>
2024-09-09 19:07:43 -0300

    implements -- Footer & header & homePage --

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared.module';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
